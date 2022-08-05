fa0c76aead14f9c966eb9d7d38953273183d4292
Kitsensas <hugo.magerat@gmail.com>
2022-08-05 08:28:50 +0200

    final ranking

--- Function/Ranking.py
+++ Function/Ranking.py
@@ -18,9 +18,11 @@ def new_score(player, pts):
     
     print("Updating data... Done")
 
-def show_rank(rank):
-    n = 1
-    rank.sort(key = lambda x: x[1], reverse = True)
+def show_rank():
+    with open ("data.json", "r", encoding = "utf-8") as data_file:
+        data = load(data_file)
+    rank = sorted(data, key = lambda k: k['pts'], reverse = True)
+    n = 0
     for i in rank:
-        print(f"{i[0]} as {i[1]} points!")
         n+=1
+        print("#", n, i['playername'], "with", i["pts"], "points")
